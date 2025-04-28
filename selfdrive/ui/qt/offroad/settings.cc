0ceb7c95cf2cd23e3e4bc6bb031116e4ea6566bf
gohokme <111654745+gohokme@users.noreply.github.com>
2025-04-29 00:51:20 +0900

    Update settings.cc

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -277,6 +277,7 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
   });
   addItem(translateBtn);
 
+  /*
   QObject::connect(uiState(), &UIState::primeTypeChanged, [this] (PrimeType type) {
     pair_device->setVisible(type == PrimeType::UNPAIRED);
   });
@@ -287,6 +288,7 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
       }
     }
   });
+  */
 
   // power buttons
   QHBoxLayout *power_layout = new QHBoxLayout();
